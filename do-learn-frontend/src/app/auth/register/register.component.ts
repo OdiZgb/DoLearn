e7cd900ffd0408c964a30d1de29454431b869570
Odi Zgb <odizgb@gmail.com>
2025-04-19 17:08:54 +0300

    Enhance ui of user profile

--- do-learn-frontend/src/app/auth/register/register.component.ts
+++ do-learn-frontend/src/app/auth/register/register.component.ts
@@ -20,7 +20,7 @@ export class RegisterComponent {
     private router: Router
   ) {
     this.registerForm = this.fb.group({
-      fullName: ['', Validators.required],
+      username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       
