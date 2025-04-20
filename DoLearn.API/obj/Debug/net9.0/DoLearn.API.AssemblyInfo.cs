0525ce3feda8cf281d89946fa20f38e3bbf1b7ba
Odi Zgb <odizgb@gmail.com>
2025-04-20 18:29:27 +0300

    Fix mig009

--- DoLearn.API/obj/Debug/net9.0/DoLearn.API.AssemblyInfo.cs
+++ DoLearn.API/obj/Debug/net9.0/DoLearn.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoLearn.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc99b1c1c2e4bbf786cc7dcf74f602cbd246ad1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022b63ed45c66e1866ab6726b9d437618effbdb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoLearn.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoLearn.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
